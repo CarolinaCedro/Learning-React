2ed5141f9782c296bdcf7cf94030a9b894938268
Cedro <yotechgirl@gmail.com>
2021-10-07 17:14:58 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [ispacient, setispacient] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:3338/datas')
+      .then((Response) => Response.json())
+      .then((data) => setispacient(data.data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Checking data ...</h1>
       </header>
-    </div>
+
+      <div className="container">
+        <section>
+          <div>
+            <span> Mostrar dados de cliente Cadastrados</span>
+            <span>
+              <button>Mostrar</button>
+            </span>
+
+            <div id="dados">
+              <h2>Dados: </h2>
+              <p>{ispacient}</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
